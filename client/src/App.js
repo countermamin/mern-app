cf992f9c6e92c9debf73df65da897a3a13288f05
Orzubek Mamiinov <orzu.maminov9@gmail.com>
2023-01-11 01:33:49 +0600

    interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React App</h1>
-      <p>Check</p>
-    </div>
-  );
-}
+import Username from './components/Username';
+import PageNotFound from './components/PageNotFound';
+import Password from './components/Password';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Register from './components/Register';
+import Reset from './components/Reset';
+
+// Root Routes
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Username></Username>,
+	},
+	{
+		path: '/register',
+		element: <Register></Register>,
+	},
+	{
+		path: '/password',
+		element: <Password></Password>,
+	},
+	{
+		path: '/profile',
+		element: <Profile></Profile>,
+	},
+	{
+		path: '/recovery',
+		element: <Recovery></Recovery>,
+	},
+	{
+		path: '/reset',
+		element: <Reset></Reset>,
+	},
+	{
+		path: '/*',
+		element: <PageNotFound></PageNotFound>,
+	},
+]);
+
+const App = () => {
+	return (
+		<main>
+			<RouterProvider router={router}></RouterProvider>
+		</main>
+	);
+};
 
 export default App;
